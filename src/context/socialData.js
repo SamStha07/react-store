26eff13ad4ffa386a214022f57b81a266ab5d396
Sam <samshrestha85@gmail.com>
2020-07-01 23:32:09 +0545

    Added Footer using context and icons

---
+++ src/context/socialData.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { FaFacebook, FaTwitter, FaLinkedin } from "react-icons/fa";
+
+export const socialData = [
+  {
+    id: 1,
+    icon: <FaFacebook className="icon" />,
+    url: "https://www.facebook.com/samstha07/",
+  },
+  {
+    id: 1,
+    icon: <FaTwitter className="icon" />,
+    url: "https://twitter.com/SamShre13644876",
+  },
+  {
+    id: 1,
+    icon: <FaLinkedin className="icon" />,
+    url: "https://www.linkedin.com/in/sam-shrestha-4798241aa/",
+  },
+];
