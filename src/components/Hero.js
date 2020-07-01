e88cffedffe2a8c09e5db0041be836b135926ee8
Sam <samshrestha85@gmail.com>
2020-07-01 16:45:14 +0545

    Added main-link class

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,36 @@
-import React, { Component } from 'react'
+import React from "react";
+import styled from "styled-components";
+import mainBcg from "../images/mainBcg.jpeg";
 
-export default class Hero extends Component {
-    render() {
-        return (
-            <div>
-                hello from hero component
-            </div>
-        )
-    }
+export default function Hero({ img, title, max, children }) {
+  return (
+    <HeroWrapper max={max} img={img}>
+      <div>
+        <h1 className="title">{title}</h1>
+        {children}
+      </div>
+    </HeroWrapper>
+  );
 }
 
+const HeroWrapper = styled.div`
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: linear-gradient(var(--primaryRGBA), var(--primaryRGBA)),
+    url(${(props) => props.img}) center/ cover no-repeat;
+  min-height: ${(props) => (props.max ? "100vh" : "60vh")};
+  color: var(--mainWhite);
+  .title {
+    padding-top: 2rem;
+    font-size: 3.5rem;
+    text-shadow: 4px 4px 2px rgba(0, 0, 0, 0.3);
+    text-transform: uppercase;
+    letter-spacing: var(--mainSpacing);
+  }
+`;
+
+Hero.defaultProps = {
+  img: mainBcg,
+};
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,14 @@
 import React from "react";
-import { ProductConsumer } from "../context/context";
 import Hero from "../components/Hero";
 import { Link } from "react-router-dom";
 
 export default function HomePage() {
   return (
     <>
-      <Hero>
-        <Link to='/products'>Products</Link>
+      <Hero title="awesome gadgets" max="true">
+        <Link style={{ margin: "2rem" }} className="main-link" to="/products">
+          Our Products
+        </Link>
       </Hero>
     </>
   );
