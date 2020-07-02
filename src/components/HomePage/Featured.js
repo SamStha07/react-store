69ddb0a31f052e6fda96a87f820a9a0d25ccd715
Sam <samshrestha85@gmail.com>
2020-07-02 20:17:26 +0545

    Added featured product section

--- src/components/HomePage/Featured.js
+++ src/components/HomePage/Featured.js
@@ -1,16 +1,30 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import Title from "../Title";
+import Product from "../Product";
+import { ProductConsumer } from "../../context/context";
 
 export default class Featured extends Component {
   render() {
     return (
-      <div>
-        <Title
-        //   style={{ marginTop: "5rem" }}
-          title="featured products"
-          center="true"
-        />
-      </div>
+      <section className="py-5">
+        <div className="container">
+          {/* title */}
+          <Title title="featured products" center="true" />
+          <div className="row">
+            <ProductConsumer>
+              {(value) => {
+                const { featuredProducts } = value;
+                return (
+                    featuredProducts.map(product =>(
+                        <Product key={product.id} product={product}></Product>
+                    ))
+                )
+              }}
+            </ProductConsumer>
+          </div>
+        </div>
+      </section>
     );
   }
 }
