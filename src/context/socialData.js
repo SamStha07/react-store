caba6ab98c417d4359f11ccf13c897b235a42b70
Sam <samshrestha85@gmail.com>
2020-07-02 14:03:45 +0545

    Updated Footer using context and icons

--- src/context/socialData.js
+++ src/context/socialData.js
@@ -8,12 +8,12 @@ export const socialData = [
     url: "https://www.facebook.com/samstha07/",
   },
   {
-    id: 1,
+    id: 2,
     icon: <FaTwitter className="icon" />,
     url: "https://twitter.com/SamShre13644876",
   },
   {
-    id: 1,
+    id: 3,
     icon: <FaLinkedin className="icon" />,
     url: "https://www.linkedin.com/in/sam-shrestha-4798241aa/",
   },
