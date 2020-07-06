75f3cf76592ddcd00bfb54aca4976edf714c0325
Sam <samshrestha85@gmail.com>
2020-07-06 13:15:11 +0545

    Added cart totals in cart component

--- src/components/CartPage/CartTotals.js
+++ src/components/CartPage/CartTotals.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import { ProductConsumer } from "../../context/context";
 
 export default function CartTotals() {
-    return (
-        <div>
-            CartTotals
-        </div>
-    )
+  return (
+    <div className="container">
+      <div className="row">
+        <ProductConsumer>
+          {(value) => {
+            const { clearCart, cartSubTotal, cartTax, cartTotal } = value;
+            return (
+              <div className="col text-title text-center my-4">
+                <button
+                  className="btn btn-outline-danger text-capitalize mb-4"
+                  onClick={clearCart}
+                >
+                  clear cart
+                </button>
+                <h3>subtotal: Rs.{cartSubTotal}</h3>
+                <h3>tax: Rs.{cartTax}</h3>
+                <h3>total: Rs.{cartTotal}</h3>
+              </div>
+            );
+          }}
+        </ProductConsumer>
+        ;
+      </div>
+    </div>
+  );
 }
