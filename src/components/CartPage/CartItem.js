dbc3b951cb9de27c17df9d7efc8b5a25bf919c64
Sam <samshrestha85@gmail.com>
2020-07-06 14:23:07 +0545

    Added items in tables in cartpage

--- src/components/CartPage/CartItem.js
+++ src/components/CartPage/CartItem.js
@@ -1,9 +1,58 @@
-import React from 'react'
+import React from "react";
+import {
+  FaTrash,
+  FaChevronCircleUp,
+  FaChevronCircleDown,
+} from "react-icons/fa";
 
-export default function CartItem() {
-    return (
-        <div>
-            cart item
+export default function CartItem({ cartItem, increment, decrement, remove }) {
+  const { id, title, price, quantity, total, image, count } = cartItem;
+  return (
+    <div className="row mt-5 mt-lg-0 text-capitalize text-center align-items-center">
+      {/* image */}
+      <div className="col-10 mx-auto col-lg-2 pb-2">
+        <img src={image} width="60" className="image-fluid" alt="product" />
+      </div>
+      {/* end image */}
+      {/* title */}
+      <div className="col-10 mx-auto col-lg-2 pb-2">
+        <span className="d-lg-none">product: </span>
+        {title}
+      </div>
+      {/* end title */}
+      {/* price */}
+      <div className="col-10 mx-auto col-lg-2 pb-2">
+        <span className="d-lg-none">price: </span>
+        Rs.{price}
+      </div>
+      {/* end price */}
+      {/* count control */}
+      <div className="col-10 mx-auto col-lg-2 my-2 my-lg-0">
+        <div className="d-flex justify-content-center">
+          <div>
+            <FaChevronCircleUp
+              className="cart-icon text-primary"
+              onClick={() => increment(id)}
+            />
+            <span className="text-title text-muted mx-3">{count}</span>
+            <FaChevronCircleDown
+              className="cart-icon text-primary"
+              onClick={() => decrement(id)}
+            />
+          </div>
         </div>
-    )
+      </div>
+      {/* end of count control */}
+      {/* remove */}
+      <div className="col-10 mx-auto col-lg-2 ">
+        <FaTrash className="text-danger cart-icon" onClick={() => remove(id)} />
+      </div>
+      {/* end remove */}
+      {/*  total */}
+      <div className="col-10 mx-auto col-lg-2 ">
+        <strong className="text-muted">item total: Rs.{total}</strong>
+      </div>
+      {/* end total */}
+    </div>
+  );
 }
--- src/components/CartPage/CartList.js
+++ src/components/CartPage/CartList.js
@@ -1,11 +1,40 @@
 import React from "react";
+import { ProductConsumer } from "../../context/context";
 import CartItem from "./CartItem";
 
 export default function CartList() {
   return (
-    <div>
-      cart-list
-      <CartItem />
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col">
+          <ProductConsumer>
+            {(value) => {
+              const { cart, increment, decrement, remove } = value;
+              if (cart.length === 0) {
+                return (
+                  <h1 className="text-title text-center my-4">
+                    your cart is currently empty
+                  </h1>
+                );
+              } else {
+                return (
+                  <>
+                    {cart.map((item) => (
+                      <CartItem
+                        key={item.id}
+                        cartItem={item}
+                        increment={increment}
+                        decrement={decrement}
+                        remove={remove}
+                      ></CartItem>
+                    ))}
+                  </>
+                );
+              }
+            }}
+          </ProductConsumer>
+        </div>
+      </div>
     </div>
   );
 }
